f3cf1556891b6eef1b654322ca1aa531a85fc444
Balint Kovacs <dratini0@gmail.com>
2017-12-01 18:44:17 +0000

    Battle-screen somewhat parodying PKMN

--- blockdraw.py
+++ blockdraw.py
@@ -1,44 +1,46 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from PIL import Image
-from sys import argv, exit
 from collections import namedtuple
+from io import StringIO
 from itertools import product
+from sys import argv
 
-image = Image.open(argv[1])
+from PIL import Image
 
 Alphabet = namedtuple("Alphabet", ["width", "height", "letters"])
 
-braille_origin = ord("⠀")
-braille_dimensions = '⠁⠈⠂⠐⠄⠠⡀⢀'
-braille_to_combine = [(0, ord(dimension) - braille_origin) for dimension in braille_dimensions]
-braille_indices = list(sum(i) + braille_origin for i in product(*braille_to_combine))
-braille_alphabet = "".join(map(chr, braille_indices))
+BRAILLE_ORIGIN = ord("⠀")
+BRAILLE_DIMENSIONS = '⠁⠈⠂⠐⠄⠠⡀⢀'
+BRAILLE_TO_COMBINE = [(0, ord(dimension) - BRAILLE_ORIGIN) for dimension in BRAILLE_DIMENSIONS]
+BRAILLE_INDICES = list(sum(i) + BRAILLE_ORIGIN for i in product(*BRAILLE_TO_COMBINE))
+BRAILLE_ALPHABET = "".join(map(chr, BRAILLE_INDICES))
 
-braille = Alphabet(2, 4, braille_alphabet)
+BRAILLE = Alphabet(2, 4, BRAILLE_ALPHABET)
 
-blockdraw_alphabet = " ▗▖▄▝▐▞▟▘▚▌▙▀▜▛█"
-blockdraw = Alphabet(2, 2, blockdraw_alphabet)
+def load_image(filename):
 
-half_blockdraw_alphabet = " ▄▀█"
-half_blockdraw = Alphabet(1, 2, half_blockdraw_alphabet)
+    alphabet = BRAILLE
 
-alphabet = braille
+    image = Image.open(filename)
+    image = image.convert("L")
+    threshold = 128
 
-image = image.convert("L")
-threshold = 128
+    result = StringIO()
 
-for tileY in range(0, image.height, alphabet.height):
-    for tileX in range(0, image.width, alphabet.width):
-        index = 0
-        for y in range(alphabet.height):
-            for x in range(alphabet.width):
-                index <<= 1
-                if tileX + x < image.width and \
-                   tileY + y < image.height and \
-                   image.getpixel((tileX + x, tileY + y)) > threshold:
-                    index += 1
-        print(alphabet.letters[index], end="")
-    print()
+    for tileY in range(0, image.height, alphabet.height):
+        for tileX in range(0, image.width, alphabet.width):
+            index = 0
+            for y in range(alphabet.height):
+                for x in range(alphabet.width):
+                    index <<= 1
+                    if tileX + x < image.width and \
+                    tileY + y < image.height and \
+                    image.getpixel((tileX + x, tileY + y)) <= threshold:
+                        index += 1
+            result.write(alphabet.letters[index])
+        result.write("\n")
+    return len(range(0, image.width, alphabet.width)), len(range(0, image.height, alphabet.height)), result.getvalue()[:-1]
 
+if __name__ == "__main__":
+    print(load_image(argv[1])[2])
